90d688fb7f71b6fdc0a8e56482aa7e3770bd7375
Sehrish <sehrmaqbool@gmail.com>
2026-07-27 21:29:50 -0700

    Add JavaScript interactivity with  form validation

--- script.js
+++ script.js
@@ -25,24 +25,68 @@ const images = [
 
 let currentImage = 0;
 const sliderImage = document.getElementById("sliderImage");
-
 const nextBtn = document.getElementById("next");
 const prevBtn = document.getElementById("prev");
 
-nextBtn.addEventListener("click", function () {
-    currentImage++;
-    if(currentImage >= images.length){
-        currentImage = 0;
-    }
-    sliderImage.src = images[currentImage];
-});
+    if(sliderImage && nextBtn && prevBtn){
+    nextBtn.addEventListener("click", function () {
+        currentImage++;
+        if(currentImage >= images.length){
+            currentImage = 0;
+        }
+        sliderImage.src = images[currentImage];
+    });
 
-prevBtn.addEventListener("click", function(){
-    currentImage--;
-    if(currentImage < 0){
+    prevBtn.addEventListener("click", function(){
+        currentImage--;
+        if(currentImage < 0){
+            currentImage = images.length - 1;
+        }
+        sliderImage.src = images[currentImage];
+    });
+}
+// Contact Form Validation
 
-        currentImage = images.length - 1;
+const contactForm = document.getElementById("contactForm");
 
-    }
-    sliderImage.src = images[currentImage];
-});
+if(contactForm){
+    contactForm.addEventListener("submit", function(event){
+        event.preventDefault();
+        const fullName = document.getElementById("fullname").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const nameError = document.getElementById("nameError");
+        const emailError = document.getElementById("emailError");
+            
+        if(fullName === ""){
+            nameError.textContent = "❌ Full Name is required.";
+            nameError.className = "error-message";
+        }
+        
+        else if(fullName.length < 3){
+            nameError.textContent = "❌ Name must be at least 3 characters.";
+            nameError.className = "error-message";
+        }
+
+        else{
+            nameError.textContent = "✅ Looks good!";
+            nameError.className = "success-message";
+        }
+
+       const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if(email === ""){
+            emailError.textContent = "❌ Email is required.";
+            emailError.className = "error-message";
+        }
+        else if(emailPattern.test(email)){
+            emailError.textContent = "✅ Valid Email.";
+            emailError.className = "success-message";
+        }
+        else{
+
+            emailError.textContent = "❌ Invalid Email.";
+            emailError.className = "error-message";
+
+        }
+    });
+}
