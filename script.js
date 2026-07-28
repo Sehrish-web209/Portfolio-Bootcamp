e77e1cd911a7222b5f91037d5895e76d1320d557
Sehrish <sehrmaqbool@gmail.com>
2026-07-27 18:54:00 -0700

    Added image slider and developer journey section using JavaScript

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ function toggleDarkMode(){
     }
 
 }
+const images = [
+    "images/slider1.jpeg",
+    "images/slider2.jpeg",
+    "images/slider3.jpeg"
+];
+
+let currentImage = 0;
+const sliderImage = document.getElementById("sliderImage");
+
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+
+nextBtn.addEventListener("click", function () {
+    currentImage++;
+    if(currentImage >= images.length){
+        currentImage = 0;
+    }
+    sliderImage.src = images[currentImage];
+});
+
+prevBtn.addEventListener("click", function(){
+    currentImage--;
+    if(currentImage < 0){
+
+        currentImage = images.length - 1;
+
+    }
+    sliderImage.src = images[currentImage];
+});
